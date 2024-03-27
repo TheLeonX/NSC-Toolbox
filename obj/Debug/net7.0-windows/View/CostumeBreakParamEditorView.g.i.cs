04293da4bdecae7f76bec6619c264ad33baee58a
TheLeonX <trapeznikov20024@yandex.ru>
2024-03-27 15:47:34 +0300

    Updated to 1.20 version

--- obj/Debug/net7.0-windows/View/CostumeBreakParamEditorView.g.i.cs
+++ obj/Debug/net7.0-windows/View/CostumeBreakParamEditorView.g.i.cs
@@ -82,7 +82,7 @@ namespace NSC_Toolbox.View {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/NSC-Toolbox;V1.0.2.3;component/view/costumebreakparameditorview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/NSC-Toolbox;V1.0.3.0;component/view/costumebreakparameditorview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\View\CostumeBreakParamEditorView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- obj/Debug/net7.0-windows/View/PRMEditorView.g.cs
+++ obj/Debug/net7.0-windows/View/PRMEditorView.g.cs
@@ -84,7 +84,7 @@ namespace NSC_Toolbox.View {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/NSC-Toolbox;V1.0.2.3;component/view/prmeditorview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/NSC-Toolbox;V1.0.3.0;component/view/prmeditorview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\View\PRMEditorView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
