ecd9b586339e9b033296b5dda5eef7ce216efc1e
TheLeonX <trapeznikov20024@yandex.ru>
2024-04-02 08:00:56 +0300

    Updated to 1.21 version

--- obj/Debug/net7.0-windows/View/MessageInfoEditorView.g.i.cs
+++ obj/Debug/net7.0-windows/View/MessageInfoEditorView.g.i.cs
@@ -82,7 +82,7 @@ namespace NSC_Toolbox.View {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/NSC-Toolbox;V1.0.3.2;component/view/messageinfoeditorview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/NSC-Toolbox;V1.0.3.3;component/view/messageinfoeditorview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\View\MessageInfoEditorView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
